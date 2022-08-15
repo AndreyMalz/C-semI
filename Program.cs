cb048458c9b0e9ac415b3ac3640e2e09f199af8a
AndreyMalz <andrmalz@gmail.com>
2022-08-15 17:00:24 +0300

    ignore

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//Console.WriteLine("Hello, World!");
+
+//Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное само на себя)
+
+Console.Clear();
+
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+//int sqrt = number * number;
+// or number *= number; 
+
+Int sqrt1 = Convert.ToInt32(Math.Pow(number, 2));
+
+Console.Write("Число в квадрате: " + sqrt1);
