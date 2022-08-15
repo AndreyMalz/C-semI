f0962d4321dfc7fbe686e4e5064b6c93008f0515
AndreyMalz <andrmalz@gmail.com>
2022-08-15 18:21:11 +0300

    Init

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("Input 1st number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input 2nd number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int square = num2 * num2;
+// можно удалить строку int square = num2... и внизу прописать 
+// if (num1 == num2 * num2)
+if (num1 == square)
+{
+    Console.Write("First number is a square of 2nd number");
+}
+else
+{
+    Console.Write("First number is not a square of second number");
+}
